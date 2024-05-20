9c259cef46ff69feb1261e0fb671a99c114a9056
javierpelaez97 <javierpelaezgimenez@gmail.com>
2024-05-20 21:49:56 +0200

    Aplicación terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,68 @@
-
 import { useState } from 'react'
+
+
 import './App.css'
 
 function App() {
 
   
-  
-  
+    const [inputValue, setInputValue] = useState('')
+    const [tipInputValue, setTipInputValue] = useState('')
+    const [result, setResult] = useState(null)
 
+    const handleChange = (event) => {
+      setInputValue(event.target.value)
+    }
+
+    const tipChangeFive = () => {
+      const newValue = inputValue * 0.05
+      setTipInputValue(newValue)
+    }
+    const tipChangeTen= ()=>{
+      const newValue = inputValue * 0.10
+      setTipInputValue(newValue)
+    }
+    const tipChangeFifteen = () => {
+      const newValue = inputValue * 0.15
+      setTipInputValue(newValue)
+    }
+    const tipChangeTwenty = () => {
+      const newValue = inputValue * 0.20
+      setTipInputValue(newValue)
+    }
+
+    const calculateSum = () => {
+      const sum = parseFloat(inputValue) + parseFloat(tipInputValue)
+      setResult(sum)
+    }
+  
   return (
     <>
-    <div>
+    <div className='title'>
       <h1>Calculadora de propinas</h1>
     </div>
-    <div>
-      <input type='number' name='price' ref='price'></input>
-
-      <div>
-        <button>5%</button>
-        <button>10%</button>
-        <button>15%</button>
-        <button>20%</button>
+    <div className='container'>
+      <label>Pon tu cuenta aquí:</label>
+      <input type='text' value={inputValue} onChange={handleChange}></input>
+      
+      <div className='items'>
+        <button onClick={tipChangeFive}>5%</button>
+        <button onClick={tipChangeTen}>10%</button>
+        <button onClick={tipChangeFifteen}>15%</button>
+        <button onClick={tipChangeTwenty}>20%</button>
       </div>
       <div>
-        <button>Calcular</button>
-        <p>Cantidad de la propina:</p>
-        <p>Total a pagar:</p>
+        
+        <p>Precio de la cuenta:{inputValue}€</p>
+        <p>Cantidad de la propina:{tipInputValue}€</p>
+        <button onClick={calculateSum}>Calcular suma</button>
+        <p className='finalPrice'>Total a pagar:{result}€</p>
       </div>
       
     </div> 
     </>
   )
 }
+
 
 export default App
